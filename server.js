009d723be27c1849ec8bb4a563530fb499ecd016
Flaff <flaffy90@gmail.com>
2017-01-12 06:00:22 +0100

    api upgrade

--- server.js
+++ server.js
@@ -10,24 +10,28 @@ var appLibrary = new AppLibrary();
 
 app.get('/steam-apps', function (request, response) {
     appLibrary.getSummary(function (data) {
+        response.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin' : '*' });
         response.end(data);
     });
 });
 
 app.get('/steam-apps/:appid', function (request, response) {
     appLibrary.getSteamApp(request.params.appid, function (data) {
+        response.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin' : '*' });
         response.end(data);
     });
 });
 
 app.get('/steam-apps/add/:appid', function (request, response) {
     appLibrary.addSteamApp(request.params.appid, function (data) {
+        response.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin' : '*' });
         response.end(data);
     });
 });
 
 app.get('/steam-apps/remove/:appid', function (request, response) {
     appLibrary.removeSteamApp(request.params.appid, function (data) {
+        response.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin' : '*' });
         response.end(data);
     });
 });
