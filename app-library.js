7351474091ef74f6f9b73c96321d46ac7bc1cd81
Flaff <flaffy90@gmail.com>
2017-01-15 01:49:16 +0100

    steam quicksearch caching, get app list on add/remove

--- app-library.js
+++ app-library.js
@@ -2,7 +2,7 @@ var http = require('http'),
     fs = require('fs'),
     htj = require('html-to-json'),
 
-    directory = 'app-library/',
+    directory = './app-library/',
     appsFile = '_apps',
     summaryFile = '_summary',
     ext = '.json',
@@ -10,10 +10,23 @@ var http = require('http'),
 
     steamHost = 'store.steampowered.com',
     steamPath = '/api/appdetails?language=polish&appids=',
-    steamQueryPath = '/search/suggest?f=games&cc=PL&l=polish&term=';
+    steamQueryPath = '/search/suggest?f=games&cc=PL&l=polish&term=',
+
+    _apps = [];
 
 
-function AppLibrary () {}
+function AppLibrary () {
+    // restore _apps
+    fs.readFile(directory + appsFile + ext, function (error, data) {
+        if (error) {
+            console.log('read error @', directory + appsFile + ext, error);
+            data = {steamApps: []};
+        } else {
+            data = JSON.parse(data);
+        }
+        _apps = data.steamApps;
+    });
+}
 
 var parseSteamAppToSummary = function (appid, data) {
     var result = {};
@@ -49,7 +62,8 @@ function parseQueryResult (data) {
             appid: data.appids[i],
             name: data.names[i],
             cover: data.images[i],
-            price: data.prices[i]
+            price: data.prices[i],
+            inLibrary: _apps.indexOf(data.appids[i]) !== -1
         });
     }
     return parsed;
@@ -74,7 +88,8 @@ AppLibrary.prototype.query = function (query, callback) {
 AppLibrary.prototype.getSummary = function (callback) {
     fs.readFile(directory + summaryFile + ext, function (error, data) {
         if(error) {
-            callback(JSON.stringify({'result':'notfound'}));
+            console.log('getSummary readFile error', error);
+            // callback(JSON.stringify({'result':'notfound'}));
         } else {
             callback(data);
         }
@@ -93,7 +108,10 @@ AppLibrary.prototype.addToSummary = function (appid, appdata) {
         appdata = parseSteamAppToSummary(appid, JSON.parse(appdata));
         data = JSON.parse(data);
         if(appdata) {
+            // add
             data.steamApps.push(appdata);
+
+            // sort
             data.steamApps.sort(function (a,b) {
                 if (a.name.toLowerCase() < b.name.toLowerCase()) {
                     return -1;
@@ -127,15 +145,18 @@ AppLibrary.prototype.removeFromSummary = function (appid) {
         }
 
         if(found !== -1) {
+            // remove
             data.steamApps.splice(found, 1);
-            data.steamApps.sort(function (a,b) {
-                if (a.name.toLowerCase() < b.name.toLowerCase()) {
-                    return -1;
-                } else if (a.name.toLowerCase() > b.name.toLowerCase()) {
-                    return 1;
-                }
-                return 0;
-            });
+
+            // sort
+            // data.steamApps.sort(function (a,b) {
+            //     if (a.name.toLowerCase() < b.name.toLowerCase()) {
+            //         return -1;
+            //     } else if (a.name.toLowerCase() > b.name.toLowerCase()) {
+            //         return 1;
+            //     }
+            //     return 0;
+            // });
             fs.writeFile(directory + summaryFile + ext, JSON.stringify(data), encoding, function (e) {
                 if(e) console.log('error updating summary (removal)', appid, e);
                 else console.log('updated summary');
@@ -144,7 +165,8 @@ AppLibrary.prototype.removeFromSummary = function (appid) {
     });
 };
 
-AppLibrary.prototype.setLocalSteamApp = function (appid, data) {
+AppLibrary.prototype.setLocalSteamApp = function (appid, data, callback) {
+    var self = this, localCallback;
     // stworz plik steamApp po appid
     fs.writeFile(directory + appid + ext, data, encoding, function (e) {
         if (e) {
@@ -159,7 +181,10 @@ AppLibrary.prototype.setLocalSteamApp = function (appid, data) {
                     data = {steamApps: []};
                 }
                 // zaktualizuj _apps - aktualizacja, dodawanie nowego appid
+
                 data = JSON.parse(data);
+                _apps = data.steamApps;
+
                 if(data.steamApps.indexOf(appid) === -1) {
                     data.steamApps.push(appid);
                     fs.writeFile(directory + appsFile + ext, JSON.stringify(data), function (e) {
@@ -167,10 +192,18 @@ AppLibrary.prototype.setLocalSteamApp = function (appid, data) {
                             console.log('failed to update', directory + appsFile + ext, ', appid:', appid);
                         } else {
                             console.log('successfully updated list:', appsFile);
+                            localCallback = function (data) {
+                                callback(JSON.stringify({result: 'success', steamApps: JSON.parse(data).steamApps}));
+                            };
+                            self.getSummary(localCallback);
                         }
                     });
                 } else {
                     console.log('app was already cached (skipping update _apps)');
+                    localCallback = function (data) {
+                        callback(JSON.stringify({result: 'success', steamApps: JSON.parse(data).steamApps}));
+                    };
+                    self.getSummary(localCallback);
                 }
             })
         }
@@ -178,6 +211,8 @@ AppLibrary.prototype.setLocalSteamApp = function (appid, data) {
 };
 
 AppLibrary.prototype.removeSteamApp = function (appid, callback) {
+    var localCallback, self = this;
+
     fs.unlink(directory + appid + ext, function (error) {
         if(error) console.log('error removing file', appid, '(not found)');
     });
@@ -186,8 +221,11 @@ AppLibrary.prototype.removeSteamApp = function (appid, callback) {
         if(error) {
             console.log('read error @', directory + appsFile + ext, error);
         }
+
         // zaktualizuj _apps - aktualizacja, dodawanie nowego appid
         data = JSON.parse(data);
+        _apps = data.steamApps;
+
         if(data.steamApps.indexOf(appid) !== -1) {
             data.steamApps.splice(data.steamApps.indexOf(appid), 1);
             fs.writeFile(directory + appsFile + ext, JSON.stringify(data), function (e) {
@@ -195,33 +233,36 @@ AppLibrary.prototype.removeSteamApp = function (appid, callback) {
                     console.log('failed to update', directory + appsFile + ext, ', appid:', appid);
                 } else {
                     console.log('successfully updated list (removal of '+appid+'):', appsFile);
+
+                    localCallback = function (data) {
+                        callback(JSON.stringify({result: 'success', steamApps: JSON.parse(data).steamApps}));
+                    };
+                    self.getSummary(localCallback);
                 }
             });
         } else {
             console.log('app was already removed (skipping update _apps)');
-            callback(JSON.stringify({'result': 'notfound'}));
+
+            localCallback = function (data) {
+                callback(JSON.stringify({result: 'success', steamApps: JSON.parse(data).steamApps}));
+            };
+            self.getSummary(localCallback);
         }
     })
 };
 
 
 AppLibrary.prototype.addSteamApp = function (appid, callback) {
-    var self = this;
+    var self = this, localCallback;
     fs.readFile(directory + appid + ext, encoding, function (error, data) {
         if (error) {
             console.log('not found, downloading:', appid);
             self.fetchSteamApp(appid, function (data) {
                 self.addToSummary(appid, data);
-                self.setLocalSteamApp(appid, data);
-                // callback(data);
-                var localCallback = function (data) {
-                    callback({result: 'success', steamApps: data});
-                };
-                self.getSummary(localCallback);
+                self.setLocalSteamApp(appid, data, callback);
             });
         } else {
             console.log('found local copy of', appid);
-            self.getSummary(callback);
         }
     });
 };
